80081be108675e3491ebade0aa5151a628134adc
Erick Ferreira Thomaz <80769325+ErickFThomaz@users.noreply.github.com>
2021-06-08 09:05:41 -0300

    Update Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,28 +1,23 @@
 package proa.java.exercicios;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Scanner;
 
 public class Ex1 {
 
-    private final static Logger logger = LoggerFactory.getLogger(Ex1.class);
-
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
 
-        logger.info("Digite o valor: ");
+       System.out.println("Digite o valor: ");
         var n = scanner.nextInt();
 
 
         if (n < 0) {
-            logger.info("O valor é negativo");
+            System.out.println("O valor é negativo");
         } else if (n > 0) {
-            logger.info("O valor é positivo");
+            System.out.println("O valor é positivo");
         } else if (n == 0) {
-            logger.info("O valor é 0");
+            System.out.println("O valor é 0");
         }
     }
 }
